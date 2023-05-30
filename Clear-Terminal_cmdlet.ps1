15c58ea9008f3550d8eeb3469693798eac15c4e6
Zliced13 <microsofteatsdick.i6idd@aleeas.com>
2023-05-30 16:30:18 -0600

    ✨ feat(✅Updated Clear-Terminal_cmdlet.ps1):

--- Clear-Terminal_cmdlet.ps1
+++ Clear-Terminal_cmdlet.ps1
@@ -32,7 +32,7 @@
 #>
 
 foreach ($Private:alias in @('cls', 'clear')) {
-    Set-Alias $Private:alias 'Clear-Terminal' -Force -Option 'AllScope', 'Constant', 'ReadOnly' -Scope 'Global' -Description 'Aliases for the Clear-Terminal cmdlet' | Out-Null
+    Set-Alias $Private:alias 'clt' -Force -Option 'AllScope', 'Constant', 'ReadOnly' -Scope 'Global' -Description 'Aliases for the Clear-Terminal cmdlet' | Out-Null
 }
 
 <#
