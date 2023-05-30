0d1fa1322f7783b952bbc3878c2e793c04affbb1
Zliced13 <microsofteatsdick.i6idd@aleeas.com>
2023-05-30 17:24:47 -0600

    ✨ feat(✅Updated Clear-Terminal_cmdlet.ps1):

--- Clear-Terminal_cmdlet.ps1
+++ Clear-Terminal_cmdlet.ps1
@@ -28,12 +28,18 @@
     Version 0.1: Added the Clear-Terminal_cmdlet.ps1 file. First release.
     Version 0.3.1: Edited and fixed the about page for Clear-Terminal again.
     Version 0.3.4: Fixed the about page for Clear-Terminal again.
-    Version 0.3.5: Fixed the about page for Clear-Terminal again.
+    Version 0.3.6: Fixed the about page for Clear-Terminal again and Improved the Clear-Terminal cmdlet.
 #>
 
 foreach ($Private:alias in @('cls', 'clear', 'clt')) {
     Set-Alias $Private:alias 'Clear-Terminal' -Force -Option 'AllScope', 'Constant', 'ReadOnly' -Scope 'Global' -Description 'Aliases for the Clear-Terminal cmdlet' | Out-Null
 }
+
+$private:alias_message = @'
+ALIASES
+    'cls', 'clear', 'clt'
+
+'@
 
 <#
 .SYNOPSIS
@@ -53,7 +59,7 @@ function Clear-Terminal {
         [switch][Alias('h')]$Help
     )
     process {
-        if ($Help) { Get-Help 'Clear-Terminal' -Full; break }
+        if ($Help) { Get-Help 'Clear-Terminal' -Full; Write-Host $private:alias_message; break }
         if (!(Get-Command 'cmd' -ErrorAction 'SilentlyContinue')) {
             Write-Host "`e[31;1mPlease run the following commands to restore 'cmd.exe' as the `e[33mClear-Terminal`e[31;1m cmdlet relies on it,`e[0m"
             Write-Host "`nDISM /Online /Cleanup-Image /ScanHealth; DISM /Online /Cleanup-Image /CheckHealth; DISM /Online /Cleanup-Image /RestoreHealth; SFC /Scannow"
