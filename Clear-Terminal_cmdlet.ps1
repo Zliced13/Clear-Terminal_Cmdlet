7e2b854270acc035b6a3b8d1567a5435dcfe3551
Zliced13 <microsofteatsdick.i6idd@aleeas.com>
2023-05-30 21:01:57 -0600

    ✨ feat(✅Updated Clear-Terminal_cmdlet.ps1):

--- Clear-Terminal_cmdlet.ps1
+++ Clear-Terminal_cmdlet.ps1
@@ -54,7 +54,7 @@ function Clear-Terminal {
         [switch][Alias('h')]$Help
     )
     process {
-        if ($Help -or $?) { Get-Help 'Clear-Terminal' -Full; Write-Host "`nALIASES`n    cls`n    clear`n    clt`n"; break }
+        if ($Help -or ($? -eq $true)) { Get-Help 'Clear-Terminal' -Full; Write-Host "`nALIASES`n    cls`n    clear`n    clt`n"; break }
         if (!(Get-Command 'cmd' -ErrorAction 'SilentlyContinue')) {
             Write-Host "`e[31;1mPlease run the following commands to restore 'cmd.exe' as the `e[33mClear-Terminal`e[31;1m cmdlet relies on it,`e[0m"
             Write-Host "`nDISM /Online /Cleanup-Image /ScanHealth; DISM /Online /Cleanup-Image /CheckHealth; DISM /Online /Cleanup-Image /RestoreHealth; SFC /Scannow"
